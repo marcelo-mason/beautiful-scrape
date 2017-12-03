526b88bcd9561084db49c2b851a4d1e27d86e6a4
phaed <phaedrus.one@gmail.com>
2017-12-03 17:02:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { select } = require('soupselect')
+const request = require('request-promise')
+const htmlparser = require('htmlparser')
+
+class BeautifulScrape {
+  async scrape(url) {
+    const response = await request.get({ url })
+    return this.parse(response)
+  }
+
+  parse(html) {
+    return new Promise(async (resolve, reject) => {
+      const handler = new htmlparser.DefaultHandler(async (err, dom) => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        const soup = select.bind(null, dom)
+        resolve({
+          find: selector => {
+            const found = soup(selector)
+            if (found.length) {
+              return found[0]
+            }
+            return []
+          },
+          findAll: selector => {
+            return soup(selector)
+          },
+          dom
+        })
+      })
+      const parser = new htmlparser.Parser(handler)
+      parser.parseComplete(html)
+    })
+  }
+}
+
+module.exports = new BeautifulScrape()
